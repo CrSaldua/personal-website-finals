d9894e99b9c9ff1abe3c462b8b6ba9f46253101f
Cristian Saldua <crsaldua@student.apc.edu.ph>
2026-02-24 21:44:58 +0800

    pusgh

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -53,6 +53,7 @@
                   <div class="progress-bar-fill" :style="{ width: stat.value + '%' }"></div>
                 </div>
               </div>
+              <p>MOVE THE BAR TO CHANGE THE DIFFICULTY</p>
             </div>
           
 
